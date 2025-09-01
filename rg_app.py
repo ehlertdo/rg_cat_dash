54aaa660e1b2734d05691d8dfdcfb4e8bc504b57
Domenik Ehlert <domenik.ehlert@proton.me>
2025-09-01 18:10:15 +0200

    Include flux plot options & update rg_cat file

--- rg_app.py
+++ rg_app.py
@@ -8,6 +8,8 @@ import pandas as pd
 # getting data
 cat = pd.read_csv('assets/rg_cat_processed.csv')
 cat['RmaxEV'] = 10**(cat['Rmax'] - 18)
+cat['logFsyn'] = np.log10(cat['Fsyn'])
+cat['logFcr'] = np.log10(cat['Fcr'])
 
 cat['class'] = cat['class'].replace({'j': 'jetted', 'u': 'unknown', 'g': 'SFG', 'p': 'point src'})
 
@@ -26,98 +28,110 @@ app.layout = [
                 html.A(children="RG Catalog", href="https://ragolu.science.ru.nl/index.html"),
                 html.Span([' from van Velzen et al. (2012) but extended with the inferred cosmic-ray luminosity and maximum rigidity calculated with the framework of Eichmann et al. (2022).']),
                 html.Br(),
-                html.Br(),
                 html.Span('created by '),
                 html.A('D. Ehlert', href='https://domenike.folk.ntnu.no', target='_blank', rel='noopener noreferrer')
             ], style={'width': '70%'}),
-        ], style={'fontWeight': 'bold', 'color': 'darkslategrey'}),
-        html.H2('Catalog Overview'),
-        dash_table.DataTable(
-            data=cat.to_dict('records'),
-            page_size=15,
-            columns=[
-                {"name": "Name", "id": "NED_id"},
-                {"name": "Type", "id": "class"},
-                {
-                    "name": "RA [°]",
-                    "id": "ra",
-                    "type": "numeric",
-                    "format": Format(precision=2, scheme=Scheme.fixed)
+        ], className='highlight'),
+        # tabl of all radio galaxies with parameters
+        html.Div([
+            # title and preamble
+            html.H2('Catalog Overview'),
+            html.Div([
+                html.P('Special key:', className='label'),
+                html.Ul(id='table-key', children=[
+                    html.Li(['F/L', html.Sub('syn'), ': synthetic 1.1GHz flux / luminosity (extrapol. from 843MHz (SUMSS) or 1.4GHz (NVSS))'], className='tight'),
+                    html.Li(['P', html.Sub('cav'), ': cavity / jet power calculated as in ', html.A('Bell et al. 2018', href='https://arxiv.org/abs/1805.01902')], className='tight'),
+                    html.Li(['R', html.Sub('max'), ': maximum rigidity inferred from the jet power'], className='tight')
+                ])
+            ]),
+            # the data table itself
+            dash_table.DataTable(
+                data=cat.to_dict('records'),
+                page_size=15,
+                columns=[
+                    {"name": "Name", "id": "NED_id"},
+                    {"name": "Type", "id": "class"},
+                    {
+                        "name": "RA [°]",
+                        "id": "ra",
+                        "type": "numeric",
+                        "format": Format(precision=2, scheme=Scheme.fixed)
+                    },
+                    {
+                        "name": "Dec [°]",
+                        "id": "dec",
+                        "type": "numeric",
+                        "format": Format(precision=2, scheme=Scheme.fixed)
+                    },
+                    {
+                        "name": "redshift",
+                        "id": "zdist",
+                        "type": "numeric",
+                        "format": Format(precision=3, scheme=Scheme.fixed)
+                    },
+                    {
+                        "name": "D [Mpc]",
+                        "id": "D",
+                        "type": "numeric",
+                        "format": Format(precision=2, scheme=Scheme.fixed)
+                    },
+                    {
+                        "name": "Fsyn [mJy]",
+                        "id": "Fsyn",
+                        "type": "numeric",
+                        "format": Format(precision=2, scheme=Scheme.fixed)
+                    },
+                    {
+                        "name": "log Lsyn [erg/s]",
+                        "id": "Lsyn",
+                        "type": "numeric",
+                        "format": Format(precision=2, scheme=Scheme.fixed)
+                    },
+                    {
+                        "name": "log Pcav [erg/s]",
+                        "id": "Pcav",
+                        "type": "numeric",
+                        "format": Format(precision=2, scheme=Scheme.fixed)
+                    },
+                    {
+                        "name": "log Rmax [eV]",
+                        "id": "Rmax",
+                        "type": "numeric",
+                        "format": Format(precision=2, scheme=Scheme.fixed)
+                    },
+                ],
+                style_as_list_view=True,
+                fill_width=False,
+                style_table={'overflowX': 'auto'},
+                style_header={
+                    'backgroundColor': 'white',
+                    'fontWeight': 'bold',
+                    'whiteSpace': 'normal',
+                    'height': 'auto',
+                    'maxWidth': '7em',
                 },
-                {
-                    "name": "Dec [°]",
-                    "id": "dec",
-                    "type": "numeric",
-                    "format": Format(precision=2, scheme=Scheme.fixed)
+                style_cell={
+                    'padding': '7px',
+                    'height': 'auto',
+                    'overflow': 'hidden',
+                    'textOverflow': 'ellipsis',
+                    'maxWidth': '10em',
                 },
-                {
-                    "name": "redshift",
-                    "id": "zdist",
-                    "type": "numeric",
-                    "format": Format(precision=2, scheme=Scheme.fixed)
-                },
-                {
-                    "name": "D [Mpc]",
-                    "id": "D",
-                    "type": "numeric",
-                    "format": Format(precision=2, scheme=Scheme.fixed)
-                },
-                {
-                    "name": "log Fsyn [mJy]",
-                    "id": "Fsyn",
-                    "type": "numeric",
-                    "format": Format(precision=2, scheme=Scheme.fixed)
-                },
-                {
-                    "name": "log Lsyn [erg/s]",
-                    "id": "Lsyn",
-                    "type": "numeric",
-                    "format": Format(precision=2, scheme=Scheme.fixed)
-                },
-                {
-                    "name": "log Pcav [erg/s]",
-                    "id": "Pcav",
-                    "type": "numeric",
-                    "format": Format(precision=2, scheme=Scheme.fixed)
-                },
-                {
-                    "name": "log Rmax [eV]",
-                    "id": "Rmax",
-                    "type": "numeric",
-                    "format": Format(precision=2, scheme=Scheme.fixed)
-                },
-            ],
-            style_as_list_view=True,
-            fill_width=False,
-            style_table={'overflowX': 'auto'},
-            style_header={
-                'backgroundColor': 'white',
-                'fontWeight': 'bold',
-                'whiteSpace': 'normal',
-                'height': 'auto',
-                'maxWidth': '7em',
-            },
-            style_cell={
-                'padding': '7px',
-                'height': 'auto',
-                'overflow': 'hidden',
-                'textOverflow': 'ellipsis',
-                'maxWidth': '10em',
-            },
-            style_cell_conditional=[
-                {
-                    'if': {'column_id': c},
-                    'textAlign': 'left'
-                } for c in ['NED_id', 'class']
-            ],
-            tooltip_data=[
-                {
-                    column: {'value': str(value), 'type': 'markdown'}
-                    for column, value in row.items()
-                } for row in cat.to_dict('records')
-            ],
-            tooltip_duration=None,
-        ),
+                style_cell_conditional=[
+                    {
+                        'if': {'column_id': c},
+                        'textAlign': 'left'
+                    } for c in ['NED_id', 'class']
+                ],
+                tooltip_data=[
+                    {
+                        column: {'value': str(value), 'type': 'markdown'}
+                        for column, value in row.items()
+                    } for row in cat.to_dict('records')
+                ],
+                tooltip_duration=None,
+            ),
+        ], style={'width': '70%'}),
         html.H2('Distance-Distribution of Sources'),
         dcc.RadioItems(
             options=['All', 'Jetted', 'Unknown', 'SFG', 'Point Source'],
@@ -127,7 +141,7 @@ app.layout = [
             style={'marginTop': '4vh'}),
         html.Div([
             dcc.Graph(id='histogram-fig', style={"width": "100%", "height": "600px"})], className="ten columns"),
-        html.H2('Source Luminosity / Power / Maximum Rigidity'),
+        html.H2('Source Luminosity / Flux / Maximum Rigidity'),
         html.Div([
             html.Label(
                 'x Scale',
@@ -170,6 +184,14 @@ app.layout = [
                         'value': 'Lcr'
                     },
                     {
+                        'label': [html.Span("F"), html.Sub("syn")],
+                        'value': 'logFcr'
+                    },
+                    {
+                        'label': [html.Span("F"), html.Sub("CR")],
+                        'value': 'logFcr'
+                    },
+                    {
                         'label': [html.Span("R"), html.Sub("max")],
                         'value': 'Rmax'
                     },
@@ -186,7 +208,7 @@ app.layout = [
             dcc.Slider(
                 min=0, max=4, step=1, value=3,
                 vertical=True,
-                verticalHeight=300,
+                verticalHeight=250,
                 id="scatterplot-class-slider",
                 marks={
                     4: "All",
@@ -231,8 +253,8 @@ def update_scatterplot(yaxis_parameter, class_value, x_scale):
         mask_class = np.ones(len(cat), dtype=bool)
     else:
         mask_class = (cat['class'] == class_label)
-    fig = px.scatter(cat[mask_class], x='D', y=yaxis_parameter, color='Lsyn', color_continuous_scale='plasma', template='plotly_white', size_max=20, size='RmaxEV', custom_data={'NED_id': True, 'Lsyn': True, 'Lcr': True, 'Rmax': True}, log_x=x_scale, labels={'D': '<i>D</i><sub>lumi</sub> [Mpc]', 'Pcav': 'log <i>P</i><sub>cav</sub> [erg / s]', 'Lsyn': 'log <i>L</i><sub>syn</sub> [erg / s]', 'Lcr': 'log <i>L</i><sub>CR</sub> [erg / s]', 'Rmax': 'log <i>R</i><sub>max</sub> [eV]'})
-    fig.update_traces(marker_sizemin=3,
+    fig = px.scatter(cat[mask_class], x='D', y=yaxis_parameter, color='Lsyn', color_continuous_scale='plasma', template='plotly_white', size='RmaxEV', custom_data={'NED_id': True, 'Lsyn': True, 'Lcr': True, 'Rmax': True}, log_x=x_scale, labels={'D': '<i>D</i><sub>lumi</sub> [Mpc]', 'Pcav': 'log <i>P</i><sub>cav</sub> [erg / s]', 'Lsyn': 'log <i>L</i><sub>syn</sub> [erg / s]', 'Lcr': 'log <i>L</i><sub>CR</sub> [erg / s]', 'Rmax': 'log <i>R</i><sub>max</sub> [eV]', 'logFsyn': 'log <i>F</i><sub>syn</sub> [erg / s / cm<sup>2</sup>]', 'logFcr': 'log <i>F</i><sub>CR</sub> [erg / s / cm<sup>2</sup>]'})
+    fig.update_traces(marker_sizemin=4,
                       selector=dict(type='scatter'),
                       hovertemplate=("<b>%{customdata[0]}</b><br>" + "<i>D</i><sub>lumi</sub> [Mpc] = %{x:.2f}<br>" +
                                      "log <i>P</i><sub>cav</sub> [erg / s] = %{y:.2f}<br>" +
